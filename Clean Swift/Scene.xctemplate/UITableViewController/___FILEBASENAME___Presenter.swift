8e0e9afc9ab99adf04afaf88d5543e1e7c053480
Anders Ullnæss <andersullnass@gmail.com>
2017-03-05 17:53:22 +0100

    Added xib. Removed comments. Changed to newing up ViewModel in presenter.

--- Clean Swift/Scene.xctemplate/UITableViewController/___FILEBASENAME___Presenter.swift
+++ Clean Swift/Scene.xctemplate/UITableViewController/___FILEBASENAME___Presenter.swift
@@ -7,11 +7,7 @@ protocol ___FILEBASENAMEASIDENTIFIER___PresenterOutput: class {
 class ___FILEBASENAMEASIDENTIFIER___Presenter {
     
     weak var output: ___FILEBASENAMEASIDENTIFIER___PresenterOutput!
-    var viewModel: ___FILEBASENAMEASIDENTIFIER___ViewModel
-    
-    init(viewModel: ___FILEBASENAMEASIDENTIFIER___ViewModel) {
-        self.viewModel = viewModel
-    }
+    var viewModel = ___FILEBASENAMEASIDENTIFIER___ViewModel()
     
 }
 
